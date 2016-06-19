76348b6090c19f4f6cd031b7e8a5af54e9c3f50e
Jonny Bekkum <jonny@webcruiter.no>
2016-06-19 18:14:23 +0200

    Add project files.

---
+++ CSPLogger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSPLogger.Startup))]
+namespace CSPLogger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
